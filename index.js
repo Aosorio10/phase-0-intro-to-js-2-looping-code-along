08beabcd2a63a9f10c31386dca5dba5aa6581718
Alejandro Osorio <avot3110@hotmail.com>
2023-04-20 13:14:56 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+    let messages = [];
+    for (let name of names) {
+      let message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(10);
